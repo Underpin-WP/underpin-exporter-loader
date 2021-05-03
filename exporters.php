9322396f68013c5aeb55156d95a114ee705c52b6
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:27:31 -0700

    Fixes issues where extended plugins did not get registered items.

--- exporters.php
+++ exporters.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
+add_action( 'underpin/before_setup', function ( $instance ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'exporters', [
+		$instance->loaders()->add( 'exporters', [
 			'instance' => 'Underpin_Exporters\Abstracts\Exporter',
 			'default'  => 'Underpin_Exporters\Factories\Exporter_Instance',
 		] );
-	}
 } );
