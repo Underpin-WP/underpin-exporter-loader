384fc6400ee728e7e9cf5547f85f0fb34062f6e5
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:51:19 -0700

    Corrects underpin autoloader

--- exporters.php
+++ exporters.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter_Instance.php' );
-		$instance->loaders()->add( 'exporters', [
+		Underpin\underpin()->get( $file )->loaders()->add( 'exporters', [
 			'instance' => 'Underpin_Exporters\Abstracts\Exporter',
 			'default'  => 'Underpin_Exporters\Factories\Exporter_Instance',
 		] );
