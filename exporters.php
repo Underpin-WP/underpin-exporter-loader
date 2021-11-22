8ac016788e9321e074b42e5770b174b58d58e1bd
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:04:35 -0600

    Compat for Underpin 2.0.0

--- exporters.php
+++ exporters.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'exporters', [
+	'update' => function ( Underpin $plugin ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Exporter_Instance.php' );
-		Underpin\underpin()->get( $file, $class )->loaders()->add( 'exporters', [
+		$plugin->loaders()->add( 'exporters', [
 			'instance' => 'Underpin_Exporters\Abstracts\Exporter',
 			'default'  => 'Underpin_Exporters\Factories\Exporter_Instance',
 		] );
-}, 10, 2 );
+	},
+] ) );
